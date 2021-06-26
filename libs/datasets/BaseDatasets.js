7668372c80e018cabf6701a58cc567f3cb829a5d
Ilia Klimov <therapyspb@yandex.ru>
2021-06-26 21:04:59 +0300

    тест

--- libs/datasets/BaseDatasets.js
+++ libs/datasets/BaseDatasets.js
@@ -2,13 +2,14 @@ import fetch from "node-fetch";
 
 export default class BaseDatasets {
   constructor(logger, host = "http://datasets-server-nodejs") {
-    this.host = host;
     this.logger = logger;
+    this.host = host;
+    this.fetch = fetch;
   }
 
   async get({ path, method = null }) {
     try {
-      const response = await fetch(path);
+      const response = await this.fetch(path);
       if (!response.ok) {
         throw new Error(`Response status is not OK: ${response.status}`);
       }
@@ -25,7 +26,7 @@ export default class BaseDatasets {
 
   async post({ path, data, method = null }) {
     try {
-      const response = await fetch(path, {
+      const response = await this.fetch(path, {
         method: "post",
         body: JSON.stringify(data),
         headers: {
