8995e0ca41eaf327d7b1389c88bfb9b922e471ca
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-09-05 12:51:58 +0300

    upd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Logger from "./libs/logger.js";
 import PrStorage from "./libs/prs.js";
 import Notifier from "./libs/notify.js";
+import Room from "./libs/room.js";
 
 export { UDatasets, GDatasets, Datasets } from "./libs/datasets/index.js";
-export { Logger, PrStorage, Notifier };
+export { Logger, PrStorage, Notifier, Room };
